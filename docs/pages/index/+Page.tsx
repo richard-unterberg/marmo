f0c27b76c7b368608a8f1d4ab3cb50915bb7a501
Richard Unterberg <richard.unterberg@lipsia.digital>
2026-07-12 21:40:37 +0200

    prep

--- docs/pages/index/+Page.tsx
+++ docs/pages/index/+Page.tsx
@@ -62,58 +62,63 @@ const Page = () => {
   )
 
   return (
-    <div ref={rootRef} className="landing-code-samples" data-beasties-container>
-      <div className="flex flex-col justify-center w-full">
-        <div data-outer className="absolute top-0 left-0 w-full overflow-hidden h-[calc(55svh-14*var(--spacing))] z-1">
-          <div data-scroller className="absolute -top-60 left-0 right-0 h-[calc(85svh+16*var(--spacing))] z-0 ">
-            <img src="/bg-dark-strong.png" alt="" className="hidden dark:block absolute w-full h-full object-fill" />
-            <img src="/bg-light-strong.png" alt="" className="dark:hidden absolute w-full h-full object-fill" />
-          </div>
-          <div className="absolute top-0 left-0 h-[calc(55svh+16*var(--spacing))] w-full bg-radial-[at_50%_15%] from-base-100 to-65% dark:from-base-100 z-0" />
-        </div>
-        <div className="absolute -top-16 left-0 right-0 bg-linear-to-b from-base-100 from-10% via-transparent via-20% to-base-100 z-4 h-[calc(55svh+2*var(--spacing))]" />
-
-        <div className="mt-24 mb-12 z-5" data-hero-headline>
-          <LayoutComponent className="relative">
-            <div className="text-center mx-auto z-2 relative">
-              <div className="px-8">
-                <img
-                  src="/marmo-dark.svg"
-                  alt="marmo logo"
-                  className="mx-auto mb-8 w-12 h-12 dark:hidden"
-                  width={20}
-                  height={20}
-                />
-                <img
-                  src="/marmo-light.svg"
-                  alt="marmo logo"
-                  className="mx-auto mb-8 w-12 h-12 hidden dark:block"
-                  width={20}
-                  height={20}
-                />
-                <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold tracking-tight text-base-content">
-                  Component factory for{' '}
-                  <span className="text-nowrap underline text-shadow-lg text-shadow-primary-muted-superlight text-primary">
-                    utility-first UI
-                  </span>
-                </h1>
-                <p className="font-normal text-base-muted text-lg md:text-2xl lg:text-3xl mt-4">
-                  Typed layer for class names. For React and SolidJS.
-                </p>
-              </div>
+    <>
+      <div ref={rootRef} className="landing-code-samples" data-beasties-container>
+        <div className="flex flex-col justify-center w-full">
+          <div
+            data-outer
+            className="absolute top-0 left-0 w-full overflow-hidden h-[calc(55svh-14*var(--spacing))] z-1"
+          >
+            <div data-scroller className="absolute -top-60 left-0 right-0 h-[calc(85svh+16*var(--spacing))] z-0 ">
+              <img src="/bg-dark-strong.png" alt="" className="hidden dark:block absolute w-full h-full object-fill" />
+              <img src="/bg-light-strong.png" alt="" className="dark:hidden absolute w-full h-full object-fill" />
             </div>
-          </LayoutComponent>
-          <LayoutComponent $size="sm" className="flex gap-2 justify-center">
-            <CTAButtons />
-          </LayoutComponent>
+            <div className="absolute top-0 left-0 h-[calc(55svh+16*var(--spacing))] w-full bg-radial-[at_50%_15%] from-base-100 to-65% dark:from-base-100 z-0" />
+          </div>
+          <div className="absolute -top-16 left-0 right-0 bg-linear-to-b from-base-100 from-10% via-transparent via-20% to-base-100 z-4 h-[calc(55svh+2*var(--spacing))]" />
+
+          <div className="mt-24 mb-12 z-5" data-hero-headline>
+            <LayoutComponent className="relative">
+              <div className="text-center mx-auto z-2 relative">
+                <div className="px-8">
+                  <img
+                    src="/marmo-dark.svg"
+                    alt="marmo logo"
+                    className="mx-auto mb-8 w-12 h-12 dark:hidden"
+                    width={20}
+                    height={20}
+                  />
+                  <img
+                    src="/marmo-light.svg"
+                    alt="marmo logo"
+                    className="mx-auto mb-8 w-12 h-12 hidden dark:block"
+                    width={20}
+                    height={20}
+                  />
+                  <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold tracking-tight text-base-content">
+                    Component factory for{' '}
+                    <span className="text-nowrap underline text-shadow-lg text-shadow-primary-muted-superlight text-primary">
+                      utility-first UI
+                    </span>
+                  </h1>
+                  <p className="font-normal text-base-muted text-lg md:text-2xl lg:text-3xl mt-4">
+                    Typed layer for class names. For React and SolidJS.
+                  </p>
+                </div>
+              </div>
+            </LayoutComponent>
+            <LayoutComponent $size="sm" className="flex gap-2 justify-center">
+              <CTAButtons />
+            </LayoutComponent>
+          </div>
+          <div className="relative z-2" data-hero-code-presenter>
+            <BaseSection />
+          </div>
         </div>
-        <div className="relative z-2" data-hero-code-presenter>
-          <BaseSection />
-        </div>
-        <ExtendTransformSection />
-        <VariantsSection />
       </div>
-    </div>
+      <ExtendTransformSection />
+      <VariantsSection />
+    </>
   )
 }
 
